a8549f55fdb78b650ed4fe7b75d3406cee5f2809
Mario Garza Chapa <mariogarza@mariogarzac.lan>
2022-08-06 19:14:22 -0500

    Modified print function

--- monster.py
+++ monster.py
@@ -4,8 +4,14 @@ class Monster:
         self.monster = monster
         self.ac = ac
         self.hp = hp
+        self.maxHP = hp
         self.initiative = initiative
 
     def printMonster(self):
-       print(self.monstID, self.monster, self.ac, self.hp, self.initiative)
+        print("| {:<5}{:<20}{:<5}{:<3}/{:<8}{:<10}|".format(self.monstID, self.monster, self.ac[:2], self.hp, self.maxHP, self.initiative))
 
+    def setMonsterHealth(self, damage):
+        self.hp -= damage
+
+    def getHP(self):
+        return self.hp
