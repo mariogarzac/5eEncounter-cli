18118caadb9bdfa32479520e3f430c15a2d72a7f
Mario Garza Chapa <mariogarza@mariogarzac.local>
2022-08-08 14:56:46 -0500

    Changed setMonsterHealth method

--- monster.py
+++ monster.py
@@ -6,12 +6,21 @@ class Monster:
         self.hp = hp
         self.maxHP = hp
         self.initiative = initiative
+        self.status = True
 
     def printMonster(self):
         print("| {:<5}{:<20}{:<5}{:<3}/{:<8}{:<10}|".format(self.monstID, self.monster, self.ac[:2], self.hp, self.maxHP, self.initiative))
 
     def setMonsterHealth(self, damage):
         self.hp -= damage
+        if (self.hp < 0):
+            self.hp = 0
 
     def getHP(self):
         return self.hp
+
+    def setStatus(self, status):
+        self.status = status
+
+    def getStatus(self):
+        return self.status
